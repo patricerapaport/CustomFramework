0a4f5a80f7808bb0501f6e0454def670a2cf0fc5
Patrice Rapaport <patrice@rapaport.fr>
2022-11-04 02:56:46 +0100

    Second commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by William Rapaport on 04/11/2022.
+//
+
+import Foundation
+
+public struct SDK  {
+    public static func doSomeWork() {
+        print("doing some work ...")
+    }
+}
